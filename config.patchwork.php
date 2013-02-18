282abf78f77951e7331ffff644e6acfb095f643e
ianbogda <ianbogda@gmail.com>
2013-02-18 09:57:31 +0100

    + Add DB()x function

--- config.patchwork.php
+++ config.patchwork.php
@@ -1,5 +1,13 @@
 <?php
 
+// Database sugar
+function DB($dsn = null)
+{
+	static $db = array();
+    empty($db[$dsn]) && $db[$dsn] = adapter_DB::connect(null === $dsn ? $GLOBALS['CONFIG']['DSN'] : $dsn);
+    return $db[$dsn];
+}
+
 $CONFIG += array(
-    'DSN' => 'mysqli://user:password@localhost/database',
+	'DSN' => 'mysqli://user:password@localhost/database',
 );
